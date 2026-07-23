46dcc00f4ce817e6faf60f5ba976feff29f186d9
Felipe DBM <fadbm2004@gmail.com>
2026-07-23 14:24:52 -0500

    add Flask application setup and home page template

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+
+# Create a Flask application instances
+app = Flask(__name__)
+# Define a route for the home page
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+# Run the application
+if __name__ == "__main__":
+    app.run(debug=True)
+
