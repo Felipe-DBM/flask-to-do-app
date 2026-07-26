e0dc5c069365b636ac352bedd54954f9242923db
Felipe DBM <fadbm2004@gmail.com>
2026-07-26 14:30:47 -0500

    fix: import timezone for accurate datetime handling in Task model

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for  
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import datetime, timezone
 import os
 # Create a Flask application instances
 app = Flask(__name__)
@@ -10,6 +10,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Initialize the database
 db = SQLAlchemy(app)
+# Define a model for the tasks
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(160), nullable=False)
+    done = db.Column(db.Boolean,default=False, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc), nullable=False)
+
+
+
 
 with app.app_context():
     if not os.path.exists('todo.db'):
